e4f9b2924bdbfa42ae533448fdb954f2095faa01
Yamada Simon <ymdsmn@gmail.com>
2025-09-21 14:37:31 -0700

    add validations to the geolocation model

--- app/models/geolocation.rb
+++ app/models/geolocation.rb
@@ -1,2 +1,7 @@
 class Geolocation < ApplicationRecord
+  IP_ADDRESS_REGEXP = /\A[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\z/
+
+  validates :latitude, presence: true, numericality: { greater_than: -90, less_than_or_equal_to: 90 }
+  validates :longitude, presence: true, numericality: { greater_than: -180, less_than_or_equal_to: 180 }
+  validates :ipaddress, presence: true, format: { with: self::IP_ADDRESS_REGEXP }
 end
