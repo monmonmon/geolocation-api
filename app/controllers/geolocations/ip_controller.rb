fa1e9113b3ad0893e1fb38609fd87d7b0c530a18
Yamada Simon <ymdsmn@gmail.com>
2025-09-23 00:06:35 -0700

    modify the api passes

---
+++ app/controllers/geolocations/ip_controller.rb
@@ -0,0 +1,26 @@
+class Geolocations::IpController < ApplicationController
+  before_action :set_geolocation, only: %i[show destroy]
+
+  def show
+    render
+  end
+
+  def create
+    @geolocation = Geolocation.build_by_ipaddress(params[:ip])
+    if @geolocation&.save
+      render :show, status: :created
+    else
+      render json: @geolocation.errors, status: :bad_request
+    end
+  end
+
+  def destroy
+    @geolocation.destroy!
+  end
+
+  private
+    def set_geolocation
+      raise ActionController::BadRequest.new("invalid IP address: #{params[:ip]}") unless Resolv::IPv4::Regex.match params[:ip]
+      @geolocation = Geolocation.find_by!(ipaddress: params[:ip])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  resources :geolocations, except: [:update]
+  namespace :geolocations do
+    resources :ip, only: %i[show destroy], param: :ip, constraints: { ip: /[^\/]+/ }
+    resources :ip, only: %i[create]
+    resources :url, only: %i[show destroy], param: :url, constraints: { url: /[A-z0-9:\/\.\?#&=%_-]+/ }
+    resources :url, only: %i[create]
+  end
 end
